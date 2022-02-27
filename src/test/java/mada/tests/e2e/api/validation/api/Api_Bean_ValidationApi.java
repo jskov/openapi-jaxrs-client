e3f36d3fbb47d554db76a73ff872df3d4c1601f6
Jesper Skov <jskov@mada.dk>
2022-02-27 12:12:51 +0100

    closer

--- src/test/java/mada/tests/e2e/api/validation/api/Api_Bean_ValidationApi.java
+++ src/test/java/mada/tests/e2e/api/validation/api/Api_Bean_ValidationApi.java
@@ -11,9 +11,8 @@ package mada.tests.e2e.api.validation.api;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponseSchema;
-
 import mada.tests.e2e.api.validation.dto.Environment;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponseSchema;
 
 @javax.annotation.Generated(value = "dk.mada.jaxrs.Generator")
 @Path("/api/validation/pathparam/{env}")
