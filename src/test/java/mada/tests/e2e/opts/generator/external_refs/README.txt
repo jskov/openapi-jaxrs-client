962ff85a81c28b0f162d0c8fb00bab46500db57c
Jesper Skov <jskov@jyskebank.dk>
2022-03-01 09:20:59 +0100

    wanted

---
+++ src/test/java/mada/tests/e2e/opts/generator/external_refs/README.txt
@@ -0,0 +1,3 @@
+Tests that user can replace a DTO with a fully qualified name of an alternative implementation.
+
+The change must affect both API and DTO classes
---
+++ src/test/java/mada/tests/e2e/opts/generator/external_refs/p1/Simple.java
@@ -0,0 +1,7 @@
+package mada.tests.e2e.opts.generator.external_refs.p1;
+
+/**
+ * A replacement for the DTO Simple that would be generated from the OpenApi document.
+ */
+public class Simple   {
+}
