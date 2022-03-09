07c3a1a4515c05f09dfffa9a02997298a0f34d97
Jesper Skov <jskov@mada.dk>
2022-03-09 17:25:58 +0100

    wanted

---
+++ src/test/java/mada/tests/e2e/opts/generator/validation_api/api/Api_Bean_ValidationApi.java
@@ -0,0 +1,45 @@
+/*
+ * openapi API Title
+ * openapi API description
+ *
+ * The version of the OpenAPI document: openapi API Version
+ * Contact: openapi API contact email
+ */
+
+package mada.tests.e2e.opts.generator.validation_api.api;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import mada.tests.e2e.opts.generator.validation_api.dto.Environment;
+import mada.tests.e2e.opts.generator.validation_api.dto.Simple;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponseSchema;
+
+@javax.annotation.Generated(value = "dk.mada.jaxrs.Generator")
+@Path("/api/validation")
+public interface Api_Bean_ValidationApi {
+
+  /**
+   * apiValidationBodyPut.
+   *
+   * @param simple  (required)
+   * @return String
+   */
+  @PUT
+  @Path("/body")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  @APIResponseSchema(String.class)
+  String apiValidationBodyPut(Simple simple);
+
+  /**
+   * apiValidationPathparamEnvGet.
+   *
+   * @param env  (required)
+   * @return String
+   */
+  @GET
+  @Path("/pathparam/{env}")
+  @Produces(MediaType.APPLICATION_JSON)
+  @APIResponseSchema(String.class)
+  String apiValidationPathparamEnvGet(@PathParam("env") Environment env);
+}
