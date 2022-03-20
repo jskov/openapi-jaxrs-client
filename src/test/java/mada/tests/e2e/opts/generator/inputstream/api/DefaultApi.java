7c705ca571968b56afacbd33cd3c12fb9487afea
Jesper Skov <jskov@mada.dk>
2022-03-20 11:49:29 +0100

    want

--- src/test/java/mada/tests/e2e/opts/generator/inputstream/api/DefaultApi.java
+++ src/test/java/mada/tests/e2e/opts/generator/inputstream/api/DefaultApi.java
@@ -7,7 +7,7 @@
 package mada.tests.e2e.opts.generator.inputstream.api;
 
 import java.io.InputStream;
-
+import java.util.List;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
@@ -17,21 +17,36 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 @javax.annotation.Generated(value = "dk.mada.jaxrs.Generator")
-@Path("/employees/{userId}/photo")
+@Path("/employees/{userId}")
 public interface DefaultApi {
+
+  /**
+   * getInfo.
+   *
+   * @param userId  (required)
+   * @return List&lt;Byte&gt;
+   */
+  @GET
+  @Path("/info")
+  @Produces("application/pdf")
+  @APIResponses({
+    @APIResponse(responseCode = "default", description = "default response",
+                 content = @Content(schema = @Schema(implementation = Byte.class, type = SchemaType.ARRAY)))
+  })
+  List<Byte> getInfo(@PathParam("userId") @NotNull String userId);
 
   /**
    * getPhoto.
    *
    * @param userId  (required)
-   * @param size  (optional)
    * @return List&lt;Byte&gt;
    */
   @GET
+  @Path("/photo")
   @Produces("image/jpeg")
   @APIResponses({
     @APIResponse(responseCode = "default", description = "default response",
                  content = @Content(schema = @Schema(implementation = Byte.class, type = SchemaType.ARRAY)))
   })
-  InputStream getPhoto(@PathParam("userId") @NotNull String userId, @QueryParam("size") String size);
+  InputStream getPhoto(@PathParam("userId") @NotNull String userId);
 }
