cea97bf62e02b7f4c57d89422a76dfdc3dd0e3b8
Jesper Skov <jskov@mada.dk>
2022-04-06 07:17:12 +0200

    wanted

---
+++ src/test/java/mada/tests/e2e/dto/enums/jackson_codehaus/dto/StringIntEnum.java
@@ -0,0 +1,49 @@
+/*
+ * openapi-examples API
+ *
+ * The version of the OpenAPI document: 1.0.0-SNAPSHOT
+ */
+
+package mada.tests.e2e.dto.enums.jackson_codehaus.dto;
+
+import java.util.Objects;
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonValue;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+/**
+ * The values are digits, but the type is string. So will not be handled as integers, but are invalid Java identifiers.
+ */
+@Schema(enumeration = {"1", "2"}, type = SchemaType.STRING)
+@javax.annotation.Generated(value = "dk.mada.jaxrs.Generator")
+public enum StringIntEnum {
+  NUMBER_1("1"),
+  NUMBER_2("2");
+
+  private final String value;
+
+  StringIntEnum(String value) {
+    this.value = value;
+  }
+
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  @JsonCreator
+  public static StringIntEnum fromValue(String value) {
+    for (StringIntEnum b : StringIntEnum.values()) {
+      if (Objects.equals(b.value, value)) {
+        return b;
+      }
+    }
+    throw new IllegalArgumentException("Unexpected value '" + value + "'");
+  }
+}
