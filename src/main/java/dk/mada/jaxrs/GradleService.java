730fa6bf8c55e338bc0d23c55c265045b1806d85
Jesper Skov <jskov@mada.dk>
2022-05-13 19:44:23 +0200

    echo input

--- src/main/java/dk/mada/jaxrs/GradleService.java
+++ src/main/java/dk/mada/jaxrs/GradleService.java
@@ -9,5 +9,14 @@ public class GradleService implements GeneratorService {
     @Override
     public void generate(Path openapiDocument, Path generatorConfig, Path toDir, Properties options) {
         System.out.println("Gradle Service");
+
+        System.out.println(" OpenApi doc: " + openapiDocument);
+        System.out.println(" Config: " + generatorConfig);
+        System.out.println(" Dest dir: " + toDir);
+
+        System.out.println("Options:");
+        options.stringPropertyNames().stream()
+            .sorted()
+            .forEach(opt -> System.out.println(" " + opt + " = '" + options.getProperty(opt) + "'"));
     }
 }
