e6cb0acdcd01247095d78e88718825a1ea2a9091
Jesper Skov <jskov@mada.dk>
2022-05-14 13:37:28 +0200

    clean

--- src/main/java/dk/mada/jaxrs/GradleService.java
+++ src/main/java/dk/mada/jaxrs/GradleService.java
@@ -7,12 +7,12 @@ import dk.mada.jaxrs.gradle.GeneratorService;
 public final class GradleService implements GeneratorService {
     @Override
     public void generateClient(ClientOptions options, Path openapiDocument, Path generatorConfig, Path toDir) {
-        System.out.println("Gradle Service");
+        System.out.println("Generate client");
 
         System.out.println(" OpenApi doc: " + openapiDocument);
         System.out.println(" Config: " + generatorConfig);
         System.out.println(" Dest dir: " + toDir);
 
-        System.out.println("Options: " + options);
+        System.out.println(" Options: " + options);
     }
 }
