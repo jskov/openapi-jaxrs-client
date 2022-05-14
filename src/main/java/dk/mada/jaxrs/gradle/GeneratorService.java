f551cbd16bd7bb88bc60c314a30b27e311efbb23
Jesper Skov <jskov@mada.dk>
2022-05-14 14:07:19 +0200

    javadoc

--- src/main/java/dk/mada/jaxrs/gradle/GeneratorService.java
+++ src/main/java/dk/mada/jaxrs/gradle/GeneratorService.java
@@ -4,19 +4,45 @@ package dk.mada.jaxrs.gradle;
 
 import java.nio.file.Path;
 
+/**
+ * Generator service.
+ *
+ * Allows decoupling the service from the two invocation
+ * methods CLI and Gradle.
+ */
 public interface GeneratorService {
-    public enum GeneratorLogLevel {
-        TRACE,
+    /** Log level to use when generating code. */
+    enum GeneratorLogLevel {
+        /** Info logging. */
+        INFO,
+        /** Debug logging. */
         DEBUG,
-        INFO
+        /** Trace logging. */
+        TRACE
     }
 
-    public record ClientOptions(
+    /**
+     * Options to client generator.
+     *
+     * @param overwrite a flag to allow overwriting existing data in destination dir
+     * @param logLevel the log level to use
+     * @param skipApi a flag to allow disabling generation of API classes
+     * @param skipDto a flag to allow disabling generation of DTO classes
+     */
+    record ClientOptions(
             boolean overwrite,
             GeneratorLogLevel logLevel,
             boolean skipApi,
             boolean skipDto) {
     }
 
-    void generateClient(ClientOptions opts, Path openapiDocument, Path generatorConfig, Path toDir);
+    /**
+     * Generate client code.
+     *
+     * @param opts the caller options to control generation
+     * @param openapiDocument the OpenAPI document
+     * @param generatorConfig the generator configuration
+     * @param destinationDir the destination directory
+     */
+    void generateClient(ClientOptions opts, Path openapiDocument, Path generatorConfig, Path destinationDir);
 }
