da5c72cea35b0dd9d84cfc07b566954fb8a77b82
Jesper Skov <jskov@mada.dk>
2022-05-26 12:33:44 +0200

    add output as source sets

--- modules/gradle-plugin/src/main/java/dk/mada/jaxrs/gradle/JaxrsPlugin.java
+++ modules/gradle-plugin/src/main/java/dk/mada/jaxrs/gradle/JaxrsPlugin.java
@@ -3,7 +3,13 @@ package dk.mada.jaxrs.gradle;
 import org.gradle.api.NamedDomainObjectContainer;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.file.Directory;
 import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.plugins.ExtensionContainer;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.SourceSetContainer;
 
 import dk.mada.jaxrs.gradle.client.GenerateClient;
 
@@ -24,23 +30,32 @@ public class JaxrsPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         ObjectFactory objects = project.getObjects();
-        JaxrsPluginExtension extension = project.getExtensions().create("jaxrs", JaxrsPluginExtension.class);
+        ExtensionContainer extensions = project.getExtensions();
+        JaxrsPluginExtension jaxrsExtension = extensions.create("jaxrs", JaxrsPluginExtension.class);
+        
+        project.getPlugins().apply(JavaPlugin.class);
+        SourceSetContainer sourceSets = extensions.getByType(SourceSetContainer.class);
+        SourceSet mainSrcSet = sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME);
         
         project.getConfigurations().create(CONFIGURATION_NAME);
-        project.getDependencies().addProvider(CONFIGURATION_NAME, extension.getGeneratorGAV());
+        project.getDependencies().addProvider(CONFIGURATION_NAME, jaxrsExtension.getGeneratorGAV());
         
         NamedDomainObjectContainer<DslClient> clientContainer =
             objects.domainObjectContainer(DslClient.class, name -> objects.newInstance(DslClient.class, name));
-        project.getExtensions().add("clients", clientContainer);
+        extensions.add("clients", clientContainer);
 
         clientContainer.all(client -> {
             String docName = client.getName();
+
+            Provider<Directory> taskOutputDir = jaxrsExtension.getRootOutputDirectory().dir(docName);
+            mainSrcSet.getJava().srcDir(taskOutputDir);
+
             project.getTasks().register("generateClient" + toPartialTaskName(docName), GenerateClient.class, t -> {
                 t.setDescription("Generates JAX-RS client " + docName);
                 t.setGroup(CLIENT_TASK_GROUP);
-                t.getOutputDirectory().set(extension.getRootOutputDirectory().dir(docName));
-                t.getOpenApiDocument().set(extension.getOpenApiDocDirectory().file(docName + ".yaml"));
-                t.getGeneratorProperties().set(extension.getOpenApiDocDirectory().file(docName + ".properties"));
+                t.getOutputDirectory().set(taskOutputDir);
+                t.getOpenApiDocument().set(jaxrsExtension.getOpenApiDocDirectory().file(docName + ".yaml"));
+                t.getGeneratorProperties().set(jaxrsExtension.getOpenApiDocDirectory().file(docName + ".properties"));
             });
         });
     }
