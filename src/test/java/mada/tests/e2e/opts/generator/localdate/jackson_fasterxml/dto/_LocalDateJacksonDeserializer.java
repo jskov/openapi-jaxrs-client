bde7d417e5d913ea647e2d7e755b78914e1ff658
Jesper Skov <jskov@mada.dk>
2022-10-26 20:04:51 +0200

    indent

--- src/test/java/mada/tests/e2e/opts/generator/localdate/jackson_fasterxml/dto/_LocalDateJacksonDeserializer.java
+++ src/test/java/mada/tests/e2e/opts/generator/localdate/jackson_fasterxml/dto/_LocalDateJacksonDeserializer.java
@@ -15,8 +15,8 @@ import java.time.format.DateTimeFormatter;
 
 @javax.annotation.processing.Generated(value = "dk.mada.jaxrs.Generator")
 public class _LocalDateJacksonDeserializer extends JsonDeserializer<LocalDate> {
-	@Override
-	public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-		return LocalDate.parse(jsonParser.getText(), DateTimeFormatter.ISO_LOCAL_DATE);
-	}
+  @Override
+  public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+    return LocalDate.parse(jsonParser.getText(), DateTimeFormatter.ISO_LOCAL_DATE);
+  }
 }
