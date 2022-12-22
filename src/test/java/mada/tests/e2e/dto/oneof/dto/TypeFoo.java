ccc434cb105615b87402b6a2ad9a5298dcfc938d
Jesper Skov <jskov@jyskebank.dk>
2022-12-22 09:11:39 +0100

    too

--- src/test/java/mada/tests/e2e/dto/oneof/dto/TypeFoo.java
+++ src/test/java/mada/tests/e2e/dto/oneof/dto/TypeFoo.java
@@ -11,55 +11,18 @@ package mada.tests.e2e.dto.oneof.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import java.util.Objects;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 /**
  * TypeFoo
  */
 @JsonPropertyOrder({
-  TypeFoo.JSON_PROPERTY_VALUE_ENUM,
-  TypeFoo.JSON_PROPERTY_VALUE_INTEGER,
-  TypeFoo.JSON_PROPERTY_VALUE_STRING,
   TypeFoo.JSON_PROPERTY_VALUE_FOO_LONG
 })
 @javax.annotation.processing.Generated(value = "dk.mada.jaxrs.Generator")
-public class TypeFoo {
-  public static final String JSON_PROPERTY_VALUE_ENUM = "valueEnum";
-  @JsonProperty(JSON_PROPERTY_VALUE_ENUM)
-  private OtherEnum valueEnum;
-
+public class TypeFoo extends AbstractType {
   public static final String JSON_PROPERTY_VALUE_FOO_LONG = "valueFooLong";
   @JsonProperty(JSON_PROPERTY_VALUE_FOO_LONG)
   private Long valueFooLong;
-
-  public static final String JSON_PROPERTY_VALUE_INTEGER = "valueInteger";
-  @JsonProperty(JSON_PROPERTY_VALUE_INTEGER)
-  @Schema(required = true)
-  private Integer valueInteger;
-
-  public static final String JSON_PROPERTY_VALUE_STRING = "valueString";
-  @JsonProperty(JSON_PROPERTY_VALUE_STRING)
-  private String valueString;
-
-  public TypeFoo valueEnum(OtherEnum valueEnum) {
-    this.valueEnum = valueEnum;
-    return this;
-  }
-
-  /**
-   * Get valueEnum
-   * @return valueEnum
-   **/
-  @Valid
-  public OtherEnum getValueEnum() {
-    return valueEnum;
-  }
-
-  public void setValueEnum(OtherEnum valueEnum) {
-    this.valueEnum = valueEnum;
-  }
 
   public TypeFoo valueFooLong(Long valueFooLong) {
     this.valueFooLong = valueFooLong;
@@ -78,41 +41,6 @@ public class TypeFoo {
     this.valueFooLong = valueFooLong;
   }
 
-  public TypeFoo valueInteger(Integer valueInteger) {
-    this.valueInteger = Objects.requireNonNull(valueInteger, "Property valueInteger is required, cannot be null");
-    return this;
-  }
-
-  /**
-   * Get valueInteger
-   * @return valueInteger
-   **/
-  @NotNull
-  public Integer getValueInteger() {
-    return valueInteger;
-  }
-
-  public void setValueInteger(Integer valueInteger) {
-    this.valueInteger = Objects.requireNonNull(valueInteger, "Property valueInteger is required, cannot be null");
-  }
-
-  public TypeFoo valueString(String valueString) {
-    this.valueString = valueString;
-    return this;
-  }
-
-  /**
-   * Get valueString
-   * @return valueString
-   **/
-  public String getValueString() {
-    return valueString;
-  }
-
-  public void setValueString(String valueString) {
-    this.valueString = valueString;
-  }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -122,25 +50,21 @@ public class TypeFoo {
       return false;
     }
     TypeFoo other = (TypeFoo) o;
-    return Objects.equals(this.valueEnum, other.valueEnum) &&
-        Objects.equals(this.valueFooLong, other.valueFooLong) &&
-        Objects.equals(this.valueInteger, other.valueInteger) &&
-        Objects.equals(this.valueString, other.valueString);
+    return Objects.equals(this.valueFooLong, other.valueFooLong) &&
+        super.equals(o);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(valueEnum, valueFooLong, valueInteger, valueString);
+    return Objects.hash(valueFooLong, super.hashCode());
   }
 
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append("class TypeFoo {");
-    sb.append("\n    valueEnum: ").append(toIndentedString(valueEnum));
+    sb.append("\n    ").append(toIndentedString(super.toString()));
     sb.append("\n    valueFooLong: ").append(toIndentedString(valueFooLong));
-    sb.append("\n    valueInteger: ").append(toIndentedString(valueInteger));
-    sb.append("\n    valueString: ").append(toIndentedString(valueString));
     sb.append("\n}");
     return sb.toString();
   }
