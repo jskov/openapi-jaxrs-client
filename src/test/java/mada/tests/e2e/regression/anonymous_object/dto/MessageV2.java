618e222daff7e3f0714e249e5ad025519567ac32
Jesper Skov <jskov@jyskebank.dk>
2022-12-22 11:53:39 +0100

    new semantics ok

---
+++ src/test/java/mada/tests/e2e/regression/anonymous_object/dto/MessageV2.java
@@ -0,0 +1,75 @@
+/*
+ * Test
+ *
+ * The version of the OpenAPI document: 1.0.0
+ */
+
+package mada.tests.e2e.regression.anonymous_object.dto;
+
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import javax.json.bind.annotation.JsonbProperty;
+import javax.json.bind.annotation.JsonbPropertyOrder;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+/**
+ * MessageV2
+ */
+@JsonbPropertyOrder({
+  MessageV2.JSON_PROPERTY_SENT_AT
+})
+@javax.annotation.processing.Generated(value = "dk.mada.jaxrs.Generator")
+public class MessageV2 {
+  public static final String JSON_PROPERTY_SENT_AT = "sentAt";
+  @JsonbProperty(JSON_PROPERTY_SENT_AT)
+  @Schema(readOnly = true, description = "Time the message was sent. Automatically generated at POST.")
+  private OffsetDateTime sentAt;
+
+  public MessageV2 sentAt(OffsetDateTime sentAt) {
+    this.sentAt = sentAt;
+    return this;
+  }
+
+  /**
+   * Time the message was sent. Automatically generated at POST.
+   *
+   * @return sentAt
+   **/
+  public OffsetDateTime getSentAt() {
+    return sentAt;
+  }
+
+  public void setSentAt(OffsetDateTime sentAt) {
+    this.sentAt = sentAt;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof MessageV2)) {
+      return false;
+    }
+    MessageV2 other = (MessageV2) o;
+    return Objects.equals(this.sentAt, other.sentAt);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(sentAt);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class MessageV2 {");
+    sb.append("\n    sentAt: ").append(toIndentedString(sentAt));
+    sb.append("\n}");
+    return sb.toString();
+  }
+
+  private String toIndentedString(Object o) {
+    return Objects.toString(o).replace("\n", "\n    ");
+  }
+}
