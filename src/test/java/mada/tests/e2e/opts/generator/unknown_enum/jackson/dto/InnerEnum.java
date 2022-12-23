4a4d23b65953a9be70d2e0081a5d68f76d3d5e58
Jesper Skov <jskov@jyskebank.dk>
2022-12-23 09:01:53 +0100

    jackson

---
+++ src/test/java/mada/tests/e2e/opts/generator/unknown_enum/jackson/dto/InnerEnum.java
@@ -0,0 +1,54 @@
+/*
+ * openapi-examples API
+ *
+ * The version of the OpenAPI document: 1.0.0-SNAPSHOT
+ */
+
+package mada.tests.e2e.opts.generator.unknown_enum.jackson.dto;
+
+import javax.json.Json;
+import javax.json.JsonString;
+import javax.json.bind.adapter.JsonbAdapter;
+import javax.json.bind.annotation.JsonbTypeAdapter;
+
+/**
+ * InnerEnum
+ */
+@JsonbTypeAdapter(mada.tests.e2e.opts.generator.unknown_enum.jackson.dto.InnerEnum.InnerEnumAdapter.class)
+@javax.annotation.processing.Generated(value = "dk.mada.jaxrs.Generator")
+public enum InnerEnum {
+  I("I"),
+  J("J");
+
+  private final String value;
+
+  InnerEnum(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  public static class InnerEnumAdapter implements JsonbAdapter<InnerEnum, JsonString> {
+      @Override
+      public JsonString adaptToJson(InnerEnum e) throws Exception {
+          return Json.createValue(String.valueOf(e.value));
+      }
+
+      @Override
+      public InnerEnum adaptFromJson(JsonString value) throws Exception {
+          for (InnerEnum b : InnerEnum.values()) {
+              if (String.valueOf(b.value).equalsIgnoreCase(value.getString())) {
+                  return b;
+              }
+          }
+          throw new IllegalStateException("Unable to deserialize '" + value.getString() + "' to type InnerEnum");
+      }
+  }
+}
