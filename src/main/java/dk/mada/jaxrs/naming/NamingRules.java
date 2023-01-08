2f0216d6316bac134b4b525d54ca722a680b0c66
Jesper Skov <jskov@mada.dk>
2023-01-08 11:43:53 +0100

    Reduce code complexity in naming rules (#208)

--- src/main/java/dk/mada/jaxrs/naming/NamingRules.java
+++ src/main/java/dk/mada/jaxrs/naming/NamingRules.java
@@ -1,6 +1,9 @@
 package dk.mada.jaxrs.naming;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.UnaryOperator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,16 +17,46 @@ import org.slf4j.LoggerFactory;
  */
 public final class NamingRules {
     private static final Logger logger = LoggerFactory.getLogger(NamingRules.class);
-    /** Prefix for literal string append. */
-    private static final String APPEND = "APPEND/";
-    /** Prefix for literal string prepend. */
-    private static final String PREPEND = "PREPEND/";
-    /** Prefix for literal string input. */
-    private static final String LITERAL = "LITERAL/";
-    /** Prefix for regular expression input. */
-    private static final String REGEXP = "REGEXP/";
+    /** Pattern for syntax split of two operator arguments. */
+    private static final Pattern PATTERN_TWO_ARGS = Pattern.compile("([^/]*)/([^/]*)");
     /** Identifiers. */
     private static final Identifiers IDENTIFIERS = new Identifiers();
+
+    /**
+     * Simple operations that operate on the input string.
+     */
+    private static final Map<String, NamingRule> OPS_INPUT = Map.of(
+            // Variable name operators
+            "OPERATIONNAME-EDGE", new NamingRule("OPERATIONNAME-EDGE", IDENTIFIERS::makeValidEdgeVariableName),
+            "PARAMETERNAME-EDGE", new NamingRule("PARAMETERNAME-EDGE", IDENTIFIERS::makeValidEdgeVariableName),
+            "PROPERTYNAME-EDGE", new NamingRule("PROPERTYNAME-EDGE", IDENTIFIERS::makeValidEdgeVariableName),
+            "PROPERTYNAME", new NamingRule("PROPERTYNAME", IDENTIFIERS::makeValidVariableName),
+            "PARAMETERNAME", new NamingRule("PARAMETERNAME", IDENTIFIERS::makeValidVariableName),
+            "OPERATIONNAME", new NamingRule("OPERATIONNAME", IDENTIFIERS::makeValidVariableName),
+            // Type name operators
+            "TYPENAME-EDGE", new NamingRule("TYPENAME-EDGE", IDENTIFIERS::makeValidEdgeTypeName),
+            "TYPENAME", new NamingRule("TYPENAME", IDENTIFIERS::makeValidTypeName),
+            // Case operators
+            "UPCASE", new NamingRule("UPCASE", String::toUpperCase),
+            "DOWNCASE", new NamingRule("DOWNCASE", String::toLowerCase));
+
+    /**
+     * Operations that operate on the input string and a single operation argument.
+     */
+    private static final Map<String, Function<String, NamingRule>> OPS_INPUT_ARG = Map.of(
+            // Appends argument to input string
+            "APPEND", arg -> new NamingRule("APPEND:" + arg, s -> s + arg),
+            // Prepends argument to input string
+            "PREPEND", arg -> new NamingRule("PREPEND:" + arg, s -> arg + s),
+            // Replaces input string with argument
+            "LITERAL", arg -> new NamingRule("LITERAL:" + arg, s -> arg));
+
+    /**
+     * Operations that operate on the input string and two operation arguments.
+     */
+    private static final Map<String, BiFunction<String, String, NamingRule>> OPS_INPUT_BIARG = Map.of(
+            // Applies regular expression replacement to input string
+            "REGEXP", NamingRules::regexpOperation);
 
     private NamingRules() {
     }
@@ -53,63 +86,52 @@ public final class NamingRules {
      * @return the resulting naming rule
      */
     public static NamingRule toRule(String ruleConfiguration) {
-        String r = ruleConfiguration.trim();
-        if ("PROPERTYNAME-EDGE".equals(r) || "PARAMETERNAME-EDGE".equals(r) || "OPERATIONNAME-EDGE".equals(r)) {
-            return new NamingRule(r, IDENTIFIERS::makeValidEdgeVariableName);
-        }
-        if ("PROPERTYNAME".equals(r) || "PARAMETERNAME".equals(r) || "OPERATIONNAME".equals(r)) {
-            return new NamingRule(r, IDENTIFIERS::makeValidVariableName);
-        }
-        if ("TYPENAME-EDGE".equals(r)) {
-            return new NamingRule(r, IDENTIFIERS::makeValidEdgeTypeName);
-        }
-        if ("TYPENAME".equals(r)) {
-            return new NamingRule(r, IDENTIFIERS::makeValidTypeName);
-        }
-        if ("UPCASE".equals(r)) {
-            return new NamingRule(r, String::toUpperCase);
-        }
-        if ("DOWNCASE".equals(r)) {
-            return new NamingRule(r, String::toLowerCase);
-        }
-        if (r.startsWith(APPEND)) {
-            if (!r.endsWith("/")) {
-                throw new IllegalArgumentException("APPEND must end with /, saw: " + r);
-            }
-            return new NamingRule(r, s -> s + r.substring(APPEND.length(), r.length() - 1));
-        }
-        if (r.startsWith(PREPEND)) {
-            if (!r.endsWith("/")) {
-                throw new IllegalArgumentException("PREPEND must end with /, saw: " + r);
-            }
-            return new NamingRule(r, s -> r.substring(PREPEND.length(), r.length() - 1) + s);
-        }
-        if (r.startsWith(LITERAL)) {
-            if (!r.endsWith("/")) {
-                throw new IllegalArgumentException("LITERAL must end with /, saw: " + r);
-            }
-            return new NamingRule(r, s -> r.substring(LITERAL.length(), r.length() - 1));
-        }
-        if (r.startsWith(REGEXP)) {
-            if (!r.endsWith("/")) {
-                throw new IllegalArgumentException("REGEXP must end with /, saw: " + r);
-            }
-            String pr = r.substring(REGEXP.length());
+        String opConfig = ruleConfiguration.trim();
+        String opName = opConfig.replaceFirst("/.*", "");
 
-            Pattern prPattern = Pattern.compile("([^/]*)/([^/]*)/");
-            Matcher m = prPattern.matcher(pr);
-            if (!m.matches()) {
-                throw new IllegalArgumentException("REGEXP Bad pattern/replacement section, saw: " + pr);
-            }
-
-            String pattern = m.group(1);
-            String replacement = m.group(2);
-
-            logger.debug(" REGEXP: {} / {}", pattern, replacement);
-
-            Pattern p = Pattern.compile(pattern);
-            return new NamingRule(r, s -> p.matcher(s).replaceAll(replacement));
+        // First handle simple input operations
+        NamingRule simpleOpRule = OPS_INPUT.get(opName);
+        if (simpleOpRule != null) {
+            return simpleOpRule;
         }
-        throw new IllegalArgumentException("Unknown naming rule: '" + r + "'");
+
+        // If no match, check for operation arguments
+        if (!opConfig.contains("/")) {
+            throw new IllegalArgumentException("Unknown naming rule: '" + opConfig + "'");
+        }
+        if (!opConfig.endsWith("/")) {
+            throw new IllegalArgumentException("Operations with arguments must end with /, saw: '" + opConfig + "'");
+        }
+
+        // Look for a matching single-argument operation
+        String value = opConfig.substring(opName.length() + 1, opConfig.length() - 1);
+        Function<String, NamingRule> oneArgOperation = OPS_INPUT_ARG.get(opName);
+        if (oneArgOperation != null) {
+            logger.debug(" Single-argument operation value: '{}'", value);
+            return oneArgOperation.apply(value);
+        }
+
+        // If no single-argument operation, look for two-argument operations
+        BiFunction<String, String, NamingRule> twoArgOperation = OPS_INPUT_BIARG.get(opName);
+        if (twoArgOperation == null) {
+            throw new IllegalArgumentException("Unknown naming rule: '" + opConfig + "'");
+        }
+
+        // Split value argument into two
+        Matcher m = PATTERN_TWO_ARGS.matcher(value);
+        if (!m.matches()) {
+            throw new IllegalArgumentException("Bad input for operations with two arguments, saw: '" + opConfig + "'");
+        }
+
+        value = m.group(1);
+        String value2 = m.group(2);
+
+        logger.debug(" Bi-argument operation values: '{}' / '{}'", value, value2);
+        return twoArgOperation.apply(value, value2);
+    }
+
+    private static NamingRule regexpOperation(String pattern, String replacement) {
+        Pattern p = Pattern.compile(pattern);
+        return new NamingRule("REGEXP:" + pattern, s -> p.matcher(s).replaceAll(replacement));
     }
 }
