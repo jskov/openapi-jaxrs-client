ade4eb297ec3713a87c3acd7fb50213b45958ea2
Jesper Skov <jskov@mada.dk>
2023-01-08 10:51:08 +0100

    ok

--- src/main/java/dk/mada/jaxrs/naming/NamingRules.java
+++ src/main/java/dk/mada/jaxrs/naming/NamingRules.java
@@ -16,12 +16,6 @@ import org.slf4j.LoggerFactory;
  */
 public final class NamingRules {
     private static final Logger logger = LoggerFactory.getLogger(NamingRules.class);
-    /** Prefix for literal string append. */
-    private static final String APPEND = "APPEND/";
-    /** Prefix for literal string prepend. */
-    private static final String PREPEND = "PREPEND/";
-    /** Prefix for literal string input. */
-    private static final String LITERAL = "LITERAL/";
     /** Prefix for regular expression input. */
     private static final String REGEXP = "REGEXP/";
     /** Identifiers. */
@@ -49,22 +43,35 @@ public final class NamingRules {
     }
 
     /**
-     * Simple operations (functions) that operate on the input string.
+     * Simple operations that operate on the input string.
      */
-    private static final Map<String, Function<String, NamingRule>> SIMPLE_OPS = Map.of(
+    private static final Map<String, NamingRule> SIMPLE_OPS = Map.of(
             // Variable name operators
-            "OPERATIONNAME-EDGE", NamingRules::replaceVariableNameEdge,
-            "PARAMETERNAME-EDGE", NamingRules::replaceVariableNameEdge,
-            "PROPERTYNAME-EDGE", NamingRules::replaceVariableNameEdge,
-            "PROPERTYNAME", NamingRules::replaceVariableName,
-            "PARAMETERNAME", NamingRules::replaceVariableName,
-            "OPERATIONNAME", NamingRules::replaceVariableName,
+            "OPERATIONNAME-EDGE", new NamingRule("OPERATIONNAME-EDGE", IDENTIFIERS::makeValidEdgeVariableName),
+            "PARAMETERNAME-EDGE", new NamingRule("PARAMETERNAME-EDGE", IDENTIFIERS::makeValidEdgeVariableName),
+            "PROPERTYNAME-EDGE", new NamingRule("PROPERTYNAME-EDGE", IDENTIFIERS::makeValidEdgeVariableName),
+            "PROPERTYNAME", new NamingRule("PROPERTYNAME", IDENTIFIERS::makeValidVariableName),
+            "PARAMETERNAME", new NamingRule("PARAMETERNAME", IDENTIFIERS::makeValidVariableName),
+            "OPERATIONNAME", new NamingRule("OPERATIONNAME", IDENTIFIERS::makeValidVariableName),
             // Type name operators
-            "TYPENAME-EDGE", s -> new NamingRule(s, IDENTIFIERS::makeValidEdgeTypeName),
-            "TYPENAME", s -> new NamingRule(s, IDENTIFIERS::makeValidTypeName),
+            "TYPENAME-EDGE", new NamingRule("TYPENAME-EDGE", IDENTIFIERS::makeValidEdgeTypeName),
+            "TYPENAME", new NamingRule("TYPENAME", IDENTIFIERS::makeValidTypeName),
             // Case operators
-            "UPCASE", s -> new NamingRule(s, String::toUpperCase),
-            "DOWNCASE", s -> new NamingRule(s, String::toLowerCase));
+            "UPCASE", new NamingRule("UPCASE", String::toUpperCase),
+            "DOWNCASE", new NamingRule("DOWNCASE", String::toLowerCase));
+
+    /**
+     * Operations that operate on the input string and a single
+     * operation argument.
+     */
+    private static final Map<String, Function<String, NamingRule>> OPS_ARG = Map.of(
+            // Appends argument to input string
+            "APPEND", arg -> new NamingRule("APPEND:" + arg, s -> s + arg),
+            // Prepends argument to input string
+            "PREPEND", arg -> new NamingRule("PREPEND:" + arg, s -> arg + s),
+            // Replaces input string with argument
+            "LITERAL", arg -> new NamingRule("LITERAL:" + arg, s -> arg)
+            );
 
     /**
      * Created a single naming rules based on a single rule configuration.
@@ -73,34 +80,48 @@ public final class NamingRules {
      * @return the resulting naming rule
      */
     public static NamingRule toRule(String ruleConfiguration) {
-        String r = ruleConfiguration.trim();
-        Function<String, NamingRule> op = SIMPLE_OPS.get(r);
-        if (op != null) {
-            return op.apply(r);
+        String opConfig = ruleConfiguration.trim();
+        String opName = opConfig.replaceAll("/.*", "");
+
+        NamingRule simpleOpRule = SIMPLE_OPS.get(opName);
+        if (simpleOpRule != null) {
+            return simpleOpRule;
         }
-        if (r.startsWith(APPEND)) {
-            if (!r.endsWith("/")) {
-                throw new IllegalArgumentException("APPEND must end with /, saw: " + r);
-            }
-            return new NamingRule(r, s -> s + r.substring(APPEND.length(), r.length() - 1));
+
+        if (!opConfig.endsWith("/")) {
+            throw new IllegalArgumentException("Operations with arguments must end with /, saw: '" + opConfig + "'");
         }
-        if (r.startsWith(PREPEND)) {
-            if (!r.endsWith("/")) {
-                throw new IllegalArgumentException("PREPEND must end with /, saw: " + r);
-            }
-            return new NamingRule(r, s -> r.substring(PREPEND.length(), r.length() - 1) + s);
+
+        String oneArgValue = opConfig.substring(opName.length() + 1, opConfig.length() - 1);
+        
+        Function<String, NamingRule> oneArgOperation = OPS_ARG.get(opName);
+        if (oneArgOperation != null) {
+            return oneArgOperation.apply(oneArgValue);
         }
-        if (r.startsWith(LITERAL)) {
-            if (!r.endsWith("/")) {
-                throw new IllegalArgumentException("LITERAL must end with /, saw: " + r);
+        
+//        if (opConfig.startsWith(APPEND)) {
+//            if (!opConfig.endsWith("/")) {
+//                throw new IllegalArgumentException("APPEND must end with /, saw: " + opConfig);
+//            }
+//            return new NamingRule(opConfig, s -> s + opConfig.substring(APPEND.length(), opConfig.length() - 1));
+//        }
+//        if (opConfig.startsWith(PREPEND)) {
+//            if (!opConfig.endsWith("/")) {
+//                throw new IllegalArgumentException("PREPEND must end with /, saw: " + opConfig);
+//            }
+//            return new NamingRule(opConfig, s -> opConfig.substring(PREPEND.length(), opConfig.length() - 1) + s);
+//        }
+//        if (opConfig.startsWith(LITERAL)) {
+//            if (!opConfig.endsWith("/")) {
+//                throw new IllegalArgumentException("LITERAL must end with /, saw: " + opConfig);
+//            }
+//            return new NamingRule(opConfig, s -> opConfig.substring(LITERAL.length(), opConfig.length() - 1));
+//        }
+        if (opConfig.startsWith(REGEXP)) {
+            if (!opConfig.endsWith("/")) {
+                throw new IllegalArgumentException("REGEXP must end with /, saw: " + opConfig);
             }
-            return new NamingRule(r, s -> r.substring(LITERAL.length(), r.length() - 1));
-        }
-        if (r.startsWith(REGEXP)) {
-            if (!r.endsWith("/")) {
-                throw new IllegalArgumentException("REGEXP must end with /, saw: " + r);
-            }
-            String pr = r.substring(REGEXP.length());
+            String pr = opConfig.substring(REGEXP.length());
 
             Pattern prPattern = Pattern.compile("([^/]*)/([^/]*)/");
             Matcher m = prPattern.matcher(pr);
@@ -114,16 +135,8 @@ public final class NamingRules {
             logger.debug(" REGEXP: {} / {}", pattern, replacement);
 
             Pattern p = Pattern.compile(pattern);
-            return new NamingRule(r, s -> p.matcher(s).replaceAll(replacement));
+            return new NamingRule(opConfig, s -> p.matcher(s).replaceAll(replacement));
         }
-        throw new IllegalArgumentException("Unknown naming rule: '" + r + "'");
-    }
-
-    private static NamingRule replaceVariableNameEdge(String r) {
-        return new NamingRule(r, IDENTIFIERS::makeValidEdgeVariableName);
-    }
-
-    private static NamingRule replaceVariableName(String r) {
-        return new NamingRule(r, IDENTIFIERS::makeValidVariableName);
+        throw new IllegalArgumentException("Unknown naming rule: '" + opConfig + "'");
     }
 }
