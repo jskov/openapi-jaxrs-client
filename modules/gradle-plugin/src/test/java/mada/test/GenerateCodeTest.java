6450c020b039835efc08701d3a9d8376d8da38c7
Jesper Skov <jskov@mada.dk>
2023-01-08 12:41:25 +0100

    works

--- modules/gradle-plugin/src/test/java/mada/test/GenerateCodeTest.java
+++ modules/gradle-plugin/src/test/java/mada/test/GenerateCodeTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 /**
+ * Runs the generator on Petstore.yaml to generate code, via the Gradle plugin.
  */
 class GenerateCodeTest {
     @TempDir Path testProjectDir;
@@ -19,7 +20,12 @@ class GenerateCodeTest {
     @Test
     void canRun() throws IOException {
         createBuildFile("""
+           repositories {
+             mavenLocal()
+             mavenCentral()
+           }
            jaxrs {
+             generatorGAV = "dk.mada.jaxrs:openapi-jaxrs-client:0.0.0-SNAPSHOT"
              clients {
                petstore {
                  download("https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v3.0/petstore.yaml")
@@ -28,22 +34,26 @@ class GenerateCodeTest {
            }
            """);
         
-        createPropertiesFile(testProjectDir.resolve("src/openapi/petstore.properties"), """
-                
+        createFile(testProjectDir.resolve("src/openapi/petstore.properties"), """
+generator-api-package = dk.mada.petstore.api
+generator-dto-package = dk.mada.petstore.dto
             """);
         
         BuildResult result = GradleRunner.create()
                 .withProjectDir(testProjectDir.toFile())
-//                .withArguments("tasks")
-                .withArguments("generateClientPetstore")
+                .withArguments("-s", "generateClientPetstore")
                 .withPluginClasspath()
                 .build();
         
         assertThat(result.getOutput())
-            .contains("hello");
+            .contains("Generate client");
+        
+        assertThat(testProjectDir.resolve("src/main/java-jaxrs/petstore"))
+            .isDirectoryRecursivelyContaining("glob:**/api/PetsApi.java")
+            .isDirectoryRecursivelyContaining("glob:**/dto/Pet.java");
     }
 
-    private void createPropertiesFile(Path file, String body) throws IOException {
+    private void createFile(Path file, String body) throws IOException {
         Files.createDirectories(file.getParent());
         Files.writeString(file, body);
     }
