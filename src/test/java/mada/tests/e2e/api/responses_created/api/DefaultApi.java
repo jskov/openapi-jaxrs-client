7c94a3b9be2dcf02f3b0cc3240616b04d4c8a96c
Jesper Skov <jskov@mada.dk>
2023-02-22 12:39:49 +0100

    simpler

--- src/test/java/mada/tests/e2e/api/responses_created/api/DefaultApi.java
+++ src/test/java/mada/tests/e2e/api/responses_created/api/DefaultApi.java
@@ -16,34 +16,17 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 @javax.annotation.processing.Generated(value = "dk.mada.jaxrs.Generator")
-@Path("/")
+@Path("/create")
 public interface DefaultApi {
 
   /**
    * create.
-   *
-   * @return boolean
    */
   @PUT
-  @Path("create")
   @Produces(MediaType.APPLICATION_JSON)
   @APIResponses({
     @APIResponse(responseCode = "201", description = "OK",
                  content = @Content(schema = @Schema(implementation = String.class)))
   })
   String create();
-
-  /**
-   * getDefault.
-   *
-   * @return String
-   */
-  @GET
-  @Path("getDefault")
-  @Produces(MediaType.APPLICATION_JSON)
-  @APIResponses({
-    @APIResponse(responseCode = "default", description = "default response",
-                 content = @Content(schema = @Schema(implementation = String.class)))
-  })
-  String getDefault();
 }
