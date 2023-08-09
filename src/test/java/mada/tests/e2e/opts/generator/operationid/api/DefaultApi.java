13564235017d211e8cc348687e8b29afc0b611a3
Jesper Skov <jskov@jyskebank.dk>
2023-08-09 08:48:47 +0200

    simplify api

--- src/test/java/mada/tests/e2e/opts/generator/operationid/api/DefaultApi.java
+++ src/test/java/mada/tests/e2e/opts/generator/operationid/api/DefaultApi.java
@@ -6,14 +6,10 @@
 
 package mada.tests.e2e.opts.generator.operationid.api;
 
-import java.util.List;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
-import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponseSchema;
 
 @javax.annotation.processing.Generated(value = "dk.mada.jaxrs.Generator")
 @Path("/employees/{userId}")
@@ -23,29 +19,23 @@ public interface DefaultApi {
    * stupid_long_prefix_getInfo.
    *
    * @param userId  (required)
-   * @return List<Byte>
+   * @return String
    */
   @GET
   @Path("/info")
-  @Produces("application/pdf")
-  @APIResponses({
-    @APIResponse(responseCode = "default", description = "default response",
-                 content = @Content(schema = @Schema(implementation = Byte.class, type = SchemaType.ARRAY)))
-  })
-  List<Byte> getInfo(@PathParam("userId") @NotNull String userId);
+  @Produces(MediaType.APPLICATION_JSON)
+  @APIResponseSchema(String.class)
+  String stupid_long_prefix_getInfo(@PathParam("userId") @NotNull String userId);
 
   /**
    * stupid_long_prefix_getPhoto.
    *
    * @param userId  (required)
-   * @return List<Byte>
+   * @return String
    */
   @GET
   @Path("/photo")
-  @Produces("image/jpeg")
-  @APIResponses({
-    @APIResponse(responseCode = "default", description = "default response",
-                 content = @Content(schema = @Schema(implementation = Byte.class, type = SchemaType.ARRAY)))
-  })
-  List<Byte> getPhoto(@PathParam("userId") @NotNull String userId);
+  @Produces(MediaType.APPLICATION_JSON)
+  @APIResponseSchema(String.class)
+  String stupid_long_prefix_getPhoto(@PathParam("userId") @NotNull String userId);
 }
--- src/test/java/mada/tests/e2e/opts/generator/operator_naming/api/DefaultApi.java
+++ src/test/java/mada/tests/e2e/opts/generator/operator_naming/api/DefaultApi.java
@@ -6,14 +6,10 @@
 
 package mada.tests.e2e.opts.generator.operator_naming.api;
 
-import java.util.List;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
-import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponseSchema;
 
 @javax.annotation.processing.Generated(value = "dk.mada.jaxrs.Generator")
 @Path("/employees/{userId}")
@@ -23,29 +19,23 @@ public interface DefaultApi {
    * infoGet.
    *
    * @param userId  (required)
-   * @return List<Byte>
+   * @return String
    */
   @GET
   @Path("/info")
-  @Produces("application/pdf")
-  @APIResponses({
-    @APIResponse(responseCode = "default", description = "default response",
-                 content = @Content(schema = @Schema(implementation = Byte.class, type = SchemaType.ARRAY)))
-  })
-  List<Byte> infoGet(@PathParam("userId") @NotNull String userId);
+  @Produces(MediaType.APPLICATION_JSON)
+  @APIResponseSchema(String.class)
+  String infoGet(@PathParam("userId") @NotNull String userId);
 
   /**
    * photoGet.
    *
    * @param userId  (required)
-   * @return List<Byte>
+   * @return String
    */
   @GET
   @Path("/photo")
-  @Produces("image/jpeg")
-  @APIResponses({
-    @APIResponse(responseCode = "default", description = "default response",
-                 content = @Content(schema = @Schema(implementation = Byte.class, type = SchemaType.ARRAY)))
-  })
-  List<Byte> photoGet(@PathParam("userId") @NotNull String userId);
+  @Produces(MediaType.APPLICATION_JSON)
+  @APIResponseSchema(String.class)
+  String photoGet(@PathParam("userId") @NotNull String userId);
 }
