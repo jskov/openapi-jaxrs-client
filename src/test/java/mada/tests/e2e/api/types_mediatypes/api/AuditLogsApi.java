bc47b46f474bc37cbf89ad8fd5e01064b13cf139
Jesper Skov <jskov@mada.dk>
2023-09-03 13:00:51 +0200

    wanted

--- src/test/java/mada/tests/e2e/api/types_mediatypes/api/AuditLogsApi.java
+++ src/test/java/mada/tests/e2e/api/types_mediatypes/api/AuditLogsApi.java
@@ -9,6 +9,7 @@ package mada.tests.e2e.api.types_mediatypes.api;
 
 import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import mada.tests.e2e.api.types_mediatypes.dto.ExceptionInformation;
 import mada.tests.e2e.api.types_mediatypes.dto.LogEntry;
 import mada.tests.e2e.api.types_mediatypes.dto.LogResult;
@@ -20,7 +21,7 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 @javax.annotation.processing.Generated(value = "dk.mada.jaxrs.Generator")
-@Path("/audit-logs")
+@Path("/")
 public interface AuditLogsApi {
 
   /**
@@ -30,6 +31,7 @@ public interface AuditLogsApi {
    * @return LogResult
    */
   @POST
+  @Path("audit-logs")
   @Consumes("application/vnd.auditlog.entry+json;version=v1")
   @Produces({"application/x.auditlog.error+json;version=v1", "application/x.auditlog.result+json;version=v1", "application/x.auditlog.violations+json;version=v1"})
   @APIResponses({
@@ -42,4 +44,48 @@ public interface AuditLogsApi {
   })
   @Operation(summary = "adds an audit log")
   LogResult addEntryV1(@Valid LogEntry dto);
+
+  /**
+   * adds an audit log.
+   *
+   * @param dto  (optional)
+   * @return LogResult
+   */
+  @POST
+  @Path("audit-logs-extra-responses")
+  @Consumes("application/vnd.auditlog.entry+json;version=v1")
+  @Produces({"application/x.auditlog.error+json;version=v1", "application/x.auditlog.result+json;version=v1", "application/x.auditlog.violations+json;version=v1", MediaType.APPLICATION_JSON})
+  @APIResponses({
+    @APIResponse(responseCode = "200", description = "Successfully added the log",
+                 content = @Content(schema = @Schema(implementation = LogResult.class), mediaType = "application/x.auditlog.result+json;version=v1")),
+    @APIResponse(responseCode = "400", description = "Constraints violation",
+                 content = @Content(schema = @Schema(implementation = ViolationConstraint.class), mediaType = "application/x.auditlog.violations+json;version=v1")),
+    @APIResponse(responseCode = "401", description = "unauthorized dummy response with two types",
+                 content = @Content(schema = @Schema(implementation = String.class), mediaType = MediaType.APPLICATION_JSON)),
+    @APIResponse(responseCode = "403", description = "forbidden dummy response with one type",
+                 content = @Content(schema = @Schema(implementation = String.class), mediaType = MediaType.APPLICATION_JSON)),
+    @APIResponse(responseCode = "500", description = "Internal error or backend failure",
+                 content = @Content(schema = @Schema(implementation = ExceptionInformation.class), mediaType = "application/x.auditlog.error+json;version=v1"))
+  })
+  @Operation(summary = "adds an audit log")
+  LogResult addEntryMultipleResponses(@Valid LogEntry dto);
+
+  /**
+   * adds an audit log.
+   *
+   * @param dto  (optional)
+   * @return LogResult
+   */
+  @POST
+  @Path("multiple-responses")
+  @Consumes("application/vnd.auditlog.entry+json;version=v1")
+  @Produces({"application/x.auditlog.result+json;version=v1", "application/x.auditlog.result+json;version=v2", "application/x.auditlog.violations+json;version=v1"})
+  @APIResponses({
+    @APIResponse(responseCode = "200", description = "Successfully added the log",
+                 content = @Content(schema = @Schema(implementation = LogResult.class), mediaType = "application/x.auditlog.result+json;version=v1")),
+    @APIResponse(responseCode = "400", description = "Constraints violation",
+                 content = @Content(schema = @Schema(implementation = ViolationConstraint.class), mediaType = "application/x.auditlog.violations+json;version=v1"))
+  })
+  @Operation(summary = "adds an audit log")
+  LogResult multipleResponsesPost(@Valid LogEntry dto);
 }
