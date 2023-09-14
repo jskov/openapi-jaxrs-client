e4b8d0c885c635cf9dcd11de17aac8cd75769e41
Jesper Skov <jskov@mada.dk>
2023-09-14 18:57:58 +0200

    Add more HTTP status codes (#408)
    This fixes #405

--- src/main/java/dk/mada/jaxrs/model/api/StatusCode.java
+++ src/main/java/dk/mada/jaxrs/model/api/StatusCode.java
@@ -3,7 +3,8 @@ package dk.mada.jaxrs.model.api;
 /**
  * HTTP status codes.
  *
- * The HttpURLConnection constants in enumeration-form.
+ * The HttpURLConnection constants in enumeration-form. And then some from
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
  */
 public enum StatusCode {
     /**
@@ -12,6 +13,13 @@ public enum StatusCode {
      * @see <a href="https://swagger.io/specification/#responsesObject">OpenApi response definition</a>
      */
     HTTP_DEFAULT(0),
+
+    /** Continue. */
+    CONTINUE(100),
+    /** Switching Protocols. */
+    SWITCHING_PROTOCOLS(101),
+    /** Early hints. */
+    EARLY_HINTS(103),
 
     /** OK. */
     HTTP_OK(200),
@@ -27,6 +35,12 @@ public enum StatusCode {
     HTTP_RESET(205),
     /** Partial Content. */
     HTTP_PARTIAL(206),
+    /** Multi-Status. */
+    MULTI_STATUS(207),
+    /** Already Reported. */
+    ALREADY_REPORTED(208),
+    /** IM Used. */
+    IM_USED(226),
 
     /** Multiple Choices. */
     HTTP_MULT_CHOICE(300),
@@ -44,6 +58,8 @@ public enum StatusCode {
     HTTP_UNUSED(306),
     /** Temporary Redirect. */
     HTTP_TEMPORARY_REDIRECT(307),
+    /** Permanent Redirect. */
+    PERMANENT_REDIRECT(308),
 
     /** Bad Request. */
     HTTP_BAD_REQUEST(400),
@@ -83,6 +99,18 @@ public enum StatusCode {
     HTTP_LOCKED(423),
     /** Failed dependency. */
     HTTP_FAILED_DEPENDENCY(424),
+    /** Too early. */
+    TOO_EARLY(425),
+    /** Upgrade Required. */
+    UPGRADE_REQUIRED(426),
+    /** Precondition Required. */
+    PRECONDITION_REQUIRED(428),
+    /** Too Many Requests. */
+    TOO_MANY_REQUESTS(429),
+    /** Request Header Fields Too Large. */
+    REQUEST_HEADER_FIELDS_TOO_LARGE(431),
+    /** Unavailable For Legal Reasons. */
+    UNAVAILABLE_FOR_LEGAL_REASONS(451),
 
     /** Internal Server Error. */
     HTTP_INTERNAL_ERROR(500),
@@ -95,7 +123,17 @@ public enum StatusCode {
     /** Gateway Timeout. */
     HTTP_GATEWAY_TIMEOUT(504),
     /** HTTP Version Not Supported. */
-    HTTP_VERSION(505);
+    HTTP_VERSION(505),
+    /** Variant Also Negotiates. */
+    VARIANT_ALSO_NEGOTIATES(506),
+    /** Insufficient Storage. */
+    INSUFFICIENT_STORAGE(507),
+    /** Loop Detected. */
+    LOOP_DETECTED(508),
+    /** Not Extended. */
+    NOT_EXTENDED(510),
+    /** Network Authentication Required. */
+    NETWORK_AUTHENTICATION_REQUIRED(511);
 
     /** Status code. */
     private final int code;
