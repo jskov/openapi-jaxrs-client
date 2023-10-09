f59325a07ec3be5d5c37180554bfb3ff3b4fcb88
Jesper Skov <jskov@mada.dk>
2023-10-09 07:36:28 +0200

    missing response

---
+++ src/test/java/mada/tests/e2e/regression/plain_object/api/ApApi.java
@@ -0,0 +1,32 @@
+/*
+ * Naked Object Test
+ *
+ * The version of the OpenAPI document: v1
+ */
+
+package mada.tests.e2e.regression.plain_object.api;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import mada.tests.e2e.regression.plain_object.dto.Request;
+import mada.tests.e2e.regression.plain_object.dto.Response;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponseSchema;
+
+@javax.annotation.processing.Generated(value = "dk.mada.jaxrs.Generator")
+@Path("/v1/ap/ag")
+public interface ApApi {
+
+  /**
+   * ap_ta.
+   *
+   * @param dto  (required)
+   * @return Response
+   */
+  @PUT
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  @APIResponseSchema(Response.class)
+  Response ap_ta(@NotNull @Valid Request dto);
+}
