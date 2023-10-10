514da501a8517f732b49564d20e54e3b08b74135
Jesper Skov <jskov@mada.dk>
2023-10-10 07:18:46 +0200

    wanted

---
+++ src/test/java/mada/tests/e2e/dto/allof_single/dto/DtoB.java
@@ -0,0 +1,67 @@
+/*
+ * jboss-helloworld
+ *
+ * The version of the OpenAPI document: 1.0
+ */
+
+package mada.tests.e2e.dto.allof_single.dto;
+
+import java.util.Objects;
+import javax.json.bind.annotation.JsonbProperty;
+
+/**
+ * DtoB
+ */
+@javax.annotation.processing.Generated(value = "dk.mada.jaxrs.Generator")
+public class DtoB {
+  public static final String JSON_PROPERTY_MEMBER_B = "memberB";
+  @JsonbProperty(JSON_PROPERTY_MEMBER_B)
+  private Integer memberB;
+
+  public DtoB memberB(Integer memberB) {
+    this.memberB = memberB;
+    return this;
+  }
+
+  /**
+   * Get memberB
+   * @return memberB
+   **/
+  public Integer getMemberB() {
+    return memberB;
+  }
+
+  public void setMemberB(Integer memberB) {
+    this.memberB = memberB;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof DtoB)) {
+      return false;
+    }
+    DtoB other = (DtoB) o;
+    return Objects.equals(this.memberB, other.memberB);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(memberB);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class DtoB {");
+    sb.append("\n    memberB: ").append(toIndentedString(memberB));
+    sb.append("\n}");
+    return sb.toString();
+  }
+
+  private String toIndentedString(Object o) {
+    return Objects.toString(o).replace("\n", "\n    ");
+  }
+}
---
+++ src/test/java/mada/tests/e2e/dto/allof_single/dto/EnumDescription.java
@@ -0,0 +1,57 @@
+/*
+ * jboss-helloworld
+ *
+ * The version of the OpenAPI document: 1.0
+ */
+
+package mada.tests.e2e.dto.allof_single.dto;
+
+import javax.json.Json;
+import javax.json.JsonString;
+import javax.json.bind.adapter.JsonbAdapter;
+import javax.json.bind.annotation.JsonbTypeAdapter;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+/**
+ * an enum.
+ */
+@JsonbTypeAdapter(mada.tests.e2e.dto.allof_single.dto.EnumDescription.EnumDescriptionAdapter.class)
+@Schema(description = "an enum")
+@javax.annotation.processing.Generated(value = "dk.mada.jaxrs.Generator")
+public enum EnumDescription {
+  VALUE_A("VALUE_A"),
+  VALUE_B("VALUE_B"),
+  VALUE_C("VALUE_C");
+
+  private final String value;
+
+  EnumDescription(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  public static class EnumDescriptionAdapter implements JsonbAdapter<EnumDescription, JsonString> {
+      @Override
+      public JsonString adaptToJson(EnumDescription e) throws Exception {
+          return Json.createValue(String.valueOf(e.value));
+      }
+
+      @Override
+      public EnumDescription adaptFromJson(JsonString value) throws Exception {
+          for (EnumDescription b : EnumDescription.values()) {
+              if (String.valueOf(b.value).equalsIgnoreCase(value.getString())) {
+                  return b;
+              }
+          }
+          throw new IllegalStateException("Unable to deserialize '" + value.getString() + "' to type EnumDescription");
+      }
+  }
+}
