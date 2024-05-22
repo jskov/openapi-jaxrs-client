615e649b37ffcb249b0c387ed5f7cbc1bd2a4b41
Jesper Skov <jskov@mada.dk>
2024-05-22 18:23:43 +0200

    wanted canonical constructor for validation

---
+++ modules/generator/src/test/java/mada/tests/e2e/record/validation/nullable/dto/AdresseIdentifikationDto.java
@@ -0,0 +1,33 @@
+/*
+ * openapi API Title
+ * openapi API description
+ *
+ * The version of the OpenAPI document: openapi API Version
+ * Contact: openapi API contact email
+ */
+
+package mada.tests.e2e.record.validation.nullable.dto;
+
+import java.util.Objects;
+import java.util.UUID;
+import javax.json.bind.annotation.JsonbProperty;
+import javax.validation.constraints.NotNull;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+/**
+ * AdresseIdentifikationDto
+ */
+@javax.annotation.processing.Generated(value = "dk.mada.jaxrs.Generator")
+public record AdresseIdentifikationDto(
+  @JsonbProperty("adgangsadresseId")
+  @Schema(required = true)
+  @NotNull
+  UUID adgangsadresseId,
+
+  @JsonbProperty("adresseId")
+  @Schema(required = true, nullable = true)
+  UUID adresseId) {
+    public AdresseIdentifikationDto {
+        Objects.requireNonNull(adgangsadresseId, "Property adgangsadresseId is required, cannot be null");
+    }
+}
