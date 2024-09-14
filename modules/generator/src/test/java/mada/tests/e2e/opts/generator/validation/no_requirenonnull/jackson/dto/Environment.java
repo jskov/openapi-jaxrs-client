06ae4e4926a39f22958ae7f5e435f705c99fe5c4
Jesper Skov <jskov@mada.dk>
2024-09-14 11:12:49 +0200

    separate off test

---
+++ modules/generator/src/test/java/mada/tests/e2e/opts/generator/validation/no_requirenonnull/jackson/dto/Environment.java
@@ -0,0 +1,50 @@
+/*
+ * openapi-examples API
+ *
+ * The version of the OpenAPI document: 1.0.0-SNAPSHOT
+ */
+
+package mada.tests.e2e.opts.generator.validation.no_requirenonnull.jackson.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import java.util.Objects;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+/**
+ * Environment
+ */
+@Schema(enumeration = {"test", "qa", "production"}, type = SchemaType.STRING)
+@javax.annotation.processing.Generated(value = "dk.mada.jaxrs.Generator")
+public enum Environment {
+  TEST("test"),
+  QA("qa"),
+  PRODUCTION("production");
+
+  private final String value;
+
+  Environment(String value) {
+    this.value = value;
+  }
+
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  @JsonCreator
+  public static Environment fromValue(String value) {
+    for (Environment b : Environment.values()) {
+      if (Objects.equals(b.value, value)) {
+        return b;
+      }
+    }
+    throw new IllegalArgumentException("Unexpected value '" + value + "'");
+  }
+}
