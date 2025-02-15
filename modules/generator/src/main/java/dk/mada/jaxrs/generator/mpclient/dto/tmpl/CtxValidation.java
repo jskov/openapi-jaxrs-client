d9840df4c9a31af883fe8f7620cdff779f644c72
Jesper Skov <jskov@mada.dk>
2025-02-15 16:30:06 +0100

    rename

--- modules/generator/src/main/java/dk/mada/jaxrs/generator/mpclient/dto/tmpl/CtxValidation.java
+++ modules/generator/src/main/java/dk/mada/jaxrs/generator/mpclient/dto/tmpl/CtxValidation.java
@@ -4,5 +4,9 @@ import org.jspecify.annotations.Nullable;
 
 /**
  * Context for bean validation.
+ *
+ * @param renderedValidation validation annotations
+ * @param javadocParamComment comments to add to end of param description
+ * @param javadoc additional javadoc for accessor methods
  */
-public record CtxValidation(String renderedValidation, String javadocPropertyComment, @Nullable String javadoc) {}
+public record CtxValidation(String renderedValidation, String javadocParamComment, @Nullable String javadoc) {}
