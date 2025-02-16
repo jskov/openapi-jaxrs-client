e648111ad2e6e5e745d738e3665bda3e033d414a
Jesper Skov <jskov@mada.dk>
2025-02-16 08:23:09 +0100

    first two

--- modules/generator/src/main/java/dk/mada/jaxrs/generator/mpclient/ValidationGenerator.java
+++ modules/generator/src/main/java/dk/mada/jaxrs/generator/mpclient/ValidationGenerator.java
@@ -1,13 +1,16 @@
 package dk.mada.jaxrs.generator.mpclient;
 
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.jspecify.annotations.Nullable;
+
 import dk.mada.jaxrs.generator.mpclient.dto.tmpl.CtxValidation;
 import dk.mada.jaxrs.generator.mpclient.imports.Imports;
 import dk.mada.jaxrs.generator.mpclient.imports.ValidationApi;
 import dk.mada.jaxrs.model.Validation;
 import dk.mada.jaxrs.model.types.Type;
 import dk.mada.jaxrs.model.types.TypeContainer;
-import java.util.Optional;
-import org.jspecify.annotations.Nullable;
 
 /**
  * Validation generator.
@@ -30,6 +33,11 @@ public class ValidationGenerator {
     }
 
     private final static class State {
+        final Imports imports;
+        final Type type;
+        final boolean isNullable;
+        final boolean isRequired;
+
         private String rendered = "";
         private String propComment = "";
         private String javadoc = "";
@@ -41,6 +49,23 @@ public class ValidationGenerator {
         @Nullable String maxLength;
         @Nullable String minimum;
         @Nullable String maximum;
+        
+        public State(Imports imports, Type type, Validation validation) {
+            this.imports = imports;
+            this.type = type;
+            isNullable = validation.nullable();
+            isRequired = validation.required();
+        }
+        
+        public boolean isCompleted() {
+            return pattern == null
+                    && minItems == null
+                    && maxItems == null
+                    && minLength == null
+                    && maxLength == null
+                    && minimum == null
+                    && maximum == null;
+        }
         
         public void addValidation(String s) {
             rendered += s;
@@ -60,6 +85,17 @@ public class ValidationGenerator {
             }
             return new CtxValidation(rendered, propComment, j);
         }
+
+        public void addImport(ValidationApi typedImport) {
+            imports.add(typedImport);
+        }
+    }
+    
+    /**
+     * Transforms validation state to rendered state if applicable.
+     */
+    @FunctionalInterface
+    interface ValidationTransformer extends Function<State, State> {
     }
     
     /**
@@ -75,25 +111,9 @@ public class ValidationGenerator {
             return Optional.empty();
         }
 
-        State state = new State();
-        
-        boolean isNullable = validation.nullable();
-        boolean isRequired = validation.required();
-        if (isRequired && !isNullable) {
-            state.addValidation("@NotNull ");
-            state.addPropComment(" (not null)");
-            imports.add(ValidationApi.NOT_NULL);
-        } else {
-            state.addPropComment(" (optional)");
-            // FIXME: missing default value here
-        }
-        // Decide where to put @Valid. I expect this to be too simple...
-        if (type.isDto() || (type instanceof TypeContainer tc && tc.innerType().isDto())) {
-            state.addValidation("@Valid ");
-            imports.add(ValidationApi.VALID);
-        }
-
-        
+        State state = new State(imports, type, validation);
+        state = transformNullable(state);
+        state = transformValid(state);
         
         // pattern = validation.pattern().map(StringRenderer::encodeRegexp);
         if (validation._pattern() != null) {
@@ -154,5 +174,26 @@ public class ValidationGenerator {
 
 
         return Optional.of(state.build());
+    }
+    
+    private State transformNullable(State state) {
+        if (state.isRequired && !state.isNullable) {
+            state.addValidation("@NotNull ");
+            state.addPropComment(" (not null)");
+            state.addImport(ValidationApi.NOT_NULL);
+        } else {
+            state.addPropComment(" (optional)");
+            // FIXME: missing default value here
+        }
+        return state;
+    }
+
+    private State transformValid(State state) {
+        // Decide where to put @Valid. I expect this to be too simple...
+        if (state.type.isDto() || (state.type instanceof TypeContainer tc && tc.innerType().isDto())) {
+            state.addValidation("@Valid ");
+            state.addImport(ValidationApi.VALID);
+        }
+        return state;
     }
 }
