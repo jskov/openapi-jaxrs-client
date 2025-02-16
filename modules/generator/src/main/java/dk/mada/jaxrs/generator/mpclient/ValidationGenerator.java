a5b8662426c8a85c24cb6d80a8d7877394092c0f
Jesper Skov <jskov@mada.dk>
2025-02-16 08:58:58 +0100

    javadoc

--- modules/generator/src/main/java/dk/mada/jaxrs/generator/mpclient/ValidationGenerator.java
+++ modules/generator/src/main/java/dk/mada/jaxrs/generator/mpclient/ValidationGenerator.java
@@ -1,16 +1,15 @@
 package dk.mada.jaxrs.generator.mpclient;
 
-import java.util.Optional;
-import java.util.function.Function;
-
-import org.jspecify.annotations.Nullable;
-
 import dk.mada.jaxrs.generator.mpclient.dto.tmpl.CtxValidation;
 import dk.mada.jaxrs.generator.mpclient.imports.Imports;
 import dk.mada.jaxrs.generator.mpclient.imports.ValidationApi;
 import dk.mada.jaxrs.model.Validation;
 import dk.mada.jaxrs.model.types.Type;
 import dk.mada.jaxrs.model.types.TypeContainer;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import org.jspecify.annotations.Nullable;
 
 /**
  * Validation generator.
@@ -32,106 +31,12 @@ public class ValidationGenerator {
         this.opts = opts;
     }
 
-    private final static class State {
-        final Imports imports;
-        final Type type;
-        final boolean isNullable;
-        final boolean isRequired;
-
-        private String rendered = "";
-        private String propComment = "";
-        private String javadoc = "";
-        
-        @Nullable String pattern;
-        @Nullable String minItems;
-        @Nullable String maxItems;
-        @Nullable String minLength;
-        @Nullable String maxLength;
-        @Nullable String minimum;
-        @Nullable String maximum;
-        
-        public State(Imports imports, Type type, Validation validation) {
-            this.imports = imports;
-            this.type = type;
-            isNullable = validation.nullable();
-            isRequired = validation.required();
-            
-            if (validation._pattern() != null) {
-                pattern = StringRenderer.encodeRegexp(validation._pattern());
-            }
-            
-            if (validation._minItems() != null) {
-                minItems = Integer.toString(validation._minItems());
-            }
-            if (validation._maxItems() != null) {
-                maxItems = Integer.toString(validation._maxItems());
-            }
-
-            if (validation._minLength() != null) {
-                minLength = Integer.toString(validation._minLength());
-            }
-            if (validation._maxLength() != null) {
-                maxLength = Integer.toString(validation._maxLength());
-            }
-
-            if (validation._minimum() != null) {
-                if (type.isBigDecimal()) {
-                    minimum = "\"" + validation._minimum().toString() + "\"";
-                } else {
-                    minimum = Long.toString(validation._minimum().longValue()) + "L";
-                }
-            }
-
-            if (validation._maximum() != null) {
-                if (type.isBigDecimal()) {
-                    maximum = "\"" + validation._maximum().toString() + "\"";
-                } else {
-                    maximum = Long.toString(validation._maximum().longValue()) + "L";
-                }
-            }
-        }
-        
-        public boolean isCompleted() {
-            return pattern == null
-                    && minItems == null
-                    && maxItems == null
-                    && minLength == null
-                    && maxLength == null
-                    && minimum == null
-                    && maximum == null;
-        }
-        
-        public void addValidation(String s) {
-            rendered += s;
-        }
-        public void addPropComment(String s) {
-            propComment += s;
-        }
-        public void addJavadoc(String s) {
-            javadoc += NL + s;
-        }
-        
-        public CtxValidation build() {
-            String j = null;
-            if (!javadoc.isEmpty()) {
-                // Trim initial newline
-                j = javadoc.substring(1);
-            }
-            return new CtxValidation(rendered, propComment, j);
-        }
-
-        public void addImport(ValidationApi typedImport) {
-            imports.add(typedImport);
-        }
-    }
-    
     /**
      * Transforms validation state to rendered state if applicable.
      */
     @FunctionalInterface
-    interface ValidationTransformer extends Function<State, State> {
-    }
-    
+    interface ValidationTransformer extends Function<State, State> {}
+
     /**
      * Generate validation rendering context from type and validation information.
      *
@@ -148,17 +53,25 @@ public class ValidationGenerator {
         State state = new State(imports, type, validation);
         state = transformNullable(state);
         state = transformValid(state);
-        state = transformPattern(state);
-        state = transformSizeItems(state);
-        state = transformSizeLength(state);
-        state = transformDecimalMin(state);
-        state = transformDecimalMax(state);
-        state = transformMin(state);
-        state = transformMax(state);
 
+        for (var transformer : fallbackTransformers) {
+            if (state.isCompleted()) {
+                break;
+            }
+            state = transformer.apply(state);
+        }
         return Optional.of(state.build());
     }
-    
+
+    private List<ValidationTransformer> fallbackTransformers = List.of(
+            this::transformPattern,
+            this::transformSizeItems,
+            this::transformSizeLength,
+            this::transformDecimalMin,
+            this::transformDecimalMax,
+            this::transformMin,
+            this::transformMax);
+
     private State transformNullable(State state) {
         if (state.isRequired && !state.isNullable) {
             state.addValidation("@NotNull ");
@@ -173,7 +86,8 @@ public class ValidationGenerator {
 
     private State transformValid(State state) {
         // Decide where to put @Valid. I expect this to be too simple...
-        if (state.type.isDto() || (state.type instanceof TypeContainer tc && tc.innerType().isDto())) {
+        if (state.type.isDto()
+                || (state.type instanceof TypeContainer tc && tc.innerType().isDto())) {
             state.addValidation("@Valid ");
             state.addImport(ValidationApi.VALID);
         }
@@ -182,14 +96,14 @@ public class ValidationGenerator {
 
     private State transformPattern(State state) {
         if (state.pattern != null) {
-            state.addValidation("@Pattern(regexp = \"" + state.pattern + "\") ");;
+            state.addValidation("@Pattern(regexp = \"" + state.pattern + "\") ");
+            ;
             state.pattern = null;
             state.addImport(ValidationApi.PATTERN);
         }
         return state;
     }
 
-    
     private State transformSizeItems(State state) {
         if (state.minItems != null || state.maxItems != null) {
             state.addImport(ValidationApi.SIZE);
@@ -264,5 +178,150 @@ public class ValidationGenerator {
 
     private State transform(State state) {
         return state;
+    }
+
+    /**
+     * State of validation input while being transformed to rendered output.
+     */
+    private static final class State {
+        /** The DTO/API imports. */
+        final Imports imports;
+        /** The type the validation is for. */
+        final Type type;
+        /** The type's nullable state. */
+        final boolean isNullable;
+        /** The type's required state. */
+        final boolean isRequired;
+
+        /** The validation in rendered form. Changed as transformers operate on the state. */
+        private String rendered = "";
+        /** The javadoc property comment in rendered form. Changed as transformers operate on the state. */
+        private String propComment = "";
+        /** The javadoc comment in rendered form. Changed as transformers operate on the state. */
+        private String javadoc = "";
+
+        /** The pattern. */
+        @Nullable String pattern;
+        /** The minimum items. */
+        @Nullable String minItems;
+        /** The maximum items. */
+        @Nullable String maxItems;
+        /** The minimum length. */
+        @Nullable String minLength;
+        /** The maximum length. */
+        @Nullable String maxLength;
+        /** The minimum. */
+        @Nullable String minimum;
+        /** The maxmimum. */
+        @Nullable String maximum;
+
+        /**
+         * Creates a new instance, making initial translation of entries to string.
+         *
+         * @param imports the DTO/API imports.
+         * @param type the type
+         * @param validation the validation
+         */
+        public State(Imports imports, Type type, Validation validation) {
+            this.imports = imports;
+            this.type = type;
+            isNullable = validation.nullable();
+            isRequired = validation.required();
+
+            if (validation._pattern() != null) {
+                pattern = StringRenderer.encodeRegexp(validation._pattern());
+            }
+
+            if (validation._minItems() != null) {
+                minItems = Integer.toString(validation._minItems());
+            }
+            if (validation._maxItems() != null) {
+                maxItems = Integer.toString(validation._maxItems());
+            }
+
+            if (validation._minLength() != null) {
+                minLength = Integer.toString(validation._minLength());
+            }
+            if (validation._maxLength() != null) {
+                maxLength = Integer.toString(validation._maxLength());
+            }
+
+            if (validation._minimum() != null) {
+                if (type.isBigDecimal()) {
+                    minimum = "\"" + validation._minimum().toString() + "\"";
+                } else {
+                    minimum = Long.toString(validation._minimum().longValue()) + "L";
+                }
+            }
+
+            if (validation._maximum() != null) {
+                if (type.isBigDecimal()) {
+                    maximum = "\"" + validation._maximum().toString() + "\"";
+                } else {
+                    maximum = Long.toString(validation._maximum().longValue()) + "L";
+                }
+            }
+        }
+
+        /** {@return true if all the variable validations fields have been processed} */
+        public boolean isCompleted() {
+            return pattern == null
+                    && minItems == null
+                    && maxItems == null
+                    && minLength == null
+                    && maxLength == null
+                    && minimum == null
+                    && maximum == null;
+        }
+
+        /**
+         * Add validation part.
+         *
+         * @param part the part
+         */
+        public void addValidation(String part) {
+            rendered += part;
+        }
+
+        /**
+         * Add javadoc property comment part.
+         *
+         * @param part the part
+         */
+        public void addPropComment(String part) {
+            propComment += part;
+        }
+
+        /**
+         * Add accessor javadoc part.
+         *
+         * @param part the part
+         */
+        public void addJavadoc(String part) {
+            javadoc += NL + part;
+        }
+
+        /** {@return completed CtxValidation from the processed state} */
+        public CtxValidation build() {
+            if (!isCompleted()) {
+                throw new IllegalStateException("All state was not properly processed!");
+            }
+
+            String j = null;
+            if (!javadoc.isEmpty()) {
+                // Trim initial newline
+                j = javadoc.substring(1);
+            }
+            return new CtxValidation(rendered, propComment, j);
+        }
+
+        /**
+         * Adds import to the imports.
+         *
+         * @param typedImport the import to add
+         */
+        public void addImport(ValidationApi typedImport) {
+            imports.add(typedImport);
+        }
     }
 }
