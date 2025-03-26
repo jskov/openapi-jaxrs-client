00cf1e2169359e98c22bc145546a379a4026ba50
Jesper Skov <jskov@mada.dk>
2025-03-26 06:29:40 +0100

    off

---
+++ modules/generator/src/test/java/mada/tests/e2e/opts/generator/security/off/api/Api_TypesApi.java
@@ -0,0 +1,43 @@
+/*
+ * openapi API Title
+ * openapi API description
+ *
+ * The version of the OpenAPI document: openapi API Version
+ * Contact: email@example.com
+ */
+
+package mada.tests.e2e.opts.generator.security.off.api;
+
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+
+@javax.annotation.processing.Generated(value = "dk.mada.jaxrs.Generator")
+@Path("/api/security")
+public interface Api_TypesApi {
+
+  /**
+   * apiSecurityOffGet.
+   *
+   * @param auth  (not null)
+   */
+  @GET
+  @Path("/off")
+  @APIResponses({
+    @APIResponse(responseCode = "204", description = "No Content")
+  })
+  void apiSecurityOffGet(@HeaderParam("Authorization") @NotNull String auth);
+
+  /**
+   * apiSecurityOnGet.
+   *
+   * @param auth  (not null)
+   */
+  @GET
+  @Path("/on")
+  @APIResponses({
+    @APIResponse(responseCode = "204", description = "No Content")
+  })
+  void apiSecurityOnGet(@HeaderParam("Authorization") @NotNull String auth);
+}
