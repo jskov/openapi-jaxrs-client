4d244d456a2b4d6c7e91c36dea52fb5f83b224f7
Jesper Skov <jskov@mada.dk>
2025-03-26 07:56:26 +0100

    Do not handle as validation if part of DTO

--- modules/parser/src/main/java/dk/mada/jaxrs/openapi/TypeConverter.java
+++ modules/parser/src/main/java/dk/mada/jaxrs/openapi/TypeConverter.java
@@ -71,7 +71,8 @@ public final class TypeConverter {
     /**
      * Constructs a new type converter.
      *
-     * This operated by looking up types, creating if missing, in the types instance.
+     * This operated by looking up types, creating if missing, in the types
+     * instance.
      *
      * @param typeNames     the type names
      * @param parserTypes   the parser types
@@ -110,7 +111,8 @@ public final class TypeConverter {
     /**
      * Creates the schema reference, but overrides the required state.
      *
-     * This is used from parameters, where the required property comes from the parameter.
+     * This is used from parameters, where the required property comes from the
+     * parameter.
      *
      * @param schema  the OpenApi schema to convert
      * @param context the context of the reference
@@ -149,17 +151,20 @@ public final class TypeConverter {
             boolean isFormRef) {}
 
     /**
-     * The type mappers convert a schema configuration to a type refence if possible (or null otherwise).
+     * The type mappers convert a schema configuration to a type refence if possible
+     * (or null otherwise).
      */
     private interface TypeMapper extends Function<RefInfo, ParserTypeRef> {}
 
     /**
      * Converts a OpenApi schema to parser type reference.
      *
-     * This serves as a lazy reference to something that will eventually be resolved to a type in the model.
+     * This serves as a lazy reference to something that will eventually be resolved
+     * to a type in the model.
      *
      * @param schema        the schema
-     * @param propertyName  the name of the property the type is associated with, or null
+     * @param propertyName  the name of the property the type is associated with, or
+     *                      null
      * @param parentDtoName the name of the DTO this schema is part of, or null
      * @return the found/created parser type reference
      */
@@ -170,10 +175,12 @@ public final class TypeConverter {
     /**
      * Converts a OpenApi schema to parser type reference.
      *
-     * This serves as a lazy reference to something that will eventually be resolved to a type in the model.
+     * This serves as a lazy reference to something that will eventually be resolved
+     * to a type in the model.
      *
      * @param schema        the OpenApi schema to convert
-     * @param propertyName  the name of the property the type is associated with, or null
+     * @param propertyName  the name of the property the type is associated with, or
+     *                      null
      * @param parentDtoName the name of the DTO this schema is part of, or null
      * @param isFormRef     true if the reference is a form parameter/field.
      * @param context       the content context, or null
@@ -257,7 +264,8 @@ public final class TypeConverter {
             return null;
         }
 
-        // As of OpenApi specification V3.1 the type encodes nullable - move it to the validation
+        // As of OpenApi specification V3.1 the type encodes nullable - move it to the
+        // validation
         Validation validation = ri.validation();
         if (schemaParser.isNullable()) {
             validation = Validations.makeNullable(validation);
@@ -516,9 +524,10 @@ public final class TypeConverter {
 
     @Nullable private ParserTypeRef createSupplementalValidation(RefInfo ri) {
         Schema<?> schema = ri.schema;
-        // If no type and reference, assume it is supplemental validation
+        // If not part of a DTO, and no type and reference, assume it is supplemental validation
         // information for the other type in a ComposedSchema.
-        if (ri.schemaParser.type() == null
+        if (ri.parentDtoName == null
+                && ri.schemaParser.type() == null
                 && (schema.getProperties() == null || schema.getProperties().isEmpty())) {
             logger.trace(" - createSupplementalValidation");
             return parserRefs.of(TypeValidation.of(ri.validation), ri.validation);
@@ -563,7 +572,8 @@ public final class TypeConverter {
             return false;
         }
 
-        // Synchronization with ApiTransformer which may generate a synthetic multipart DTO
+        // Synchronization with ApiTransformer which may generate a synthetic multipart
+        // DTO
         return !context.syntheticMultipart();
     }
 
