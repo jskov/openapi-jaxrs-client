c54dad968f7384f69968e09143c99ad5b9627238
Jesper Skov <jskov@jyskebank.dk>
2026-08-11 10:21:07 +0200

    Record builder fields must all be nullable
    The builder object is born with all fields null, so they must be
    nullable.
    (fixes #1038)

--- modules/generator/src/test/java/mada/tests/e2e/opts/generator/record/builders_all/dto/CyclicB.java
+++ modules/generator/src/test/java/mada/tests/e2e/opts/generator/record/builders_all/dto/CyclicB.java
@@ -8,8 +8,11 @@
 
 package mada.tests.e2e.opts.generator.record.builders_all.dto;
 
+import java.util.Objects;
 import javax.json.bind.annotation.JsonbProperty;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.jspecify.annotations.Nullable;
 
 /**
@@ -20,10 +23,24 @@ public record CyclicB(
   @JsonbProperty("a")
   @Valid
   @Nullable
-  CyclicA a) {
+  CyclicA a,
 
+  @JsonbProperty("fieldIsNotNullable")
+  @Schema(required = true)
+  @NotNull
+  String fieldIsNotNullable,
+
+  @JsonbProperty("fieldIsNullable")
+  @Nullable
+  String fieldIsNullable) {
+
+    public CyclicB {
+      Objects.requireNonNull(fieldIsNotNullable, "Property fieldIsNotNullable is required, cannot be null");
+    }
     public static class Builder {
       private @Nullable CyclicA a;
+      private @Nullable String fieldIsNotNullable;
+      private @Nullable String fieldIsNullable;
 
       public static Builder of() {
         return new Builder();
@@ -32,6 +49,8 @@ public record CyclicB(
       public static Builder of(CyclicB from) {
         Builder o = new Builder();
         o.a = from.a();
+        o.fieldIsNotNullable = from.fieldIsNotNullable();
+        o.fieldIsNullable = from.fieldIsNullable();
         return o;
       }
 
@@ -40,9 +59,21 @@ public record CyclicB(
         return this;
       }
 
+      public Builder fieldIsNotNullable(String fieldIsNotNullable) {
+        this.fieldIsNotNullable = fieldIsNotNullable;
+        return this;
+      }
+
+      public Builder fieldIsNullable(@Nullable String fieldIsNullable) {
+        this.fieldIsNullable = fieldIsNullable;
+        return this;
+      }
+
       public CyclicB build() {
         return new CyclicB(
-                 a
+                 a,
+                 fieldIsNotNullable,
+                 fieldIsNullable
                );
       }
     }
